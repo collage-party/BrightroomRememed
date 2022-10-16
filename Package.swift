264727c63c610ec547ce4332d233f93da8a39f15
Rafi Rizwan <vi66raptor@gmail.com>
2022-10-16 15:31:48 -0700

    move package.swift up a level to support CD/CI build paths

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "Brightroom",
+  platforms: [
+    .iOS(.v13)
+  ],
+  products: [
+    .library(name: "BrightroomEngine", targets: ["BrightroomUI"]),
+    .library(name: "BrightroomUI", targets: ["BrightroomUI"]),
+  ],
+  dependencies: [
+    .package(name: "Verge", url: "https://github.com/VergeGroup/Verge.git", from: "8.19.0"),
+    .package(url: "https://github.com/FluidGroup/TransitionPatch.git", from: "1.0.3")
+  ],
+  targets: [
+    .target(
+      name: "BrightroomEngine",
+      dependencies: ["Verge"],
+      exclude: ["Info.plist"]
+    ),      
+    .target(
+      name: "BrightroomUI",
+      dependencies: ["BrightroomEngine", "Verge", "TransitionPatch"],
+      exclude: ["Info.plist"],
+      swiftSettings: [.define("SWIFT_PACKAGE_MANAGER")]
+    )
+  ],
+  swiftLanguageVersions: [.v5]
+)
